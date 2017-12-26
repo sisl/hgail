36ea51dd5f6213eb5e73a1b229458ba05aa068b4
wulfebw <wulfebw@stanford.edu>
2017-12-26 16:32:58 -0600

    fixing syntax in max_to_keep change

--- hgail/misc/utils.py
+++ hgail/misc/utils.py
@@ -266,9 +266,9 @@ def save_params(output_dir, params, epoch, max_to_keep=None):
     # delete files if in excess of max_to_keep
     if max_to_keep is not None:
         files = [os.path.join(output_dir, f) 
-            for f in os.listdir(output_dir) 
-                if os.path.isfile(os.path.join(output_dir, f)) and 
-                if 'itr_' in f]
+                for f in os.listdir(output_dir) 
+                if os.path.isfile(os.path.join(output_dir, f)) 
+                and 'itr_' in f]
         sorted_files = sorted(files, key=os.path.getmtime, reverse=True)
         if len(sorted_files) > max_to_keep:
             for filepath in sorted_files[max_to_keep:]:
