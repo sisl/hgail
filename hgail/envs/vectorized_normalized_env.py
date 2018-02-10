4084746f5a7cdc1c34c50b7c32d5b3faf8f0c0c1
wulfebw <wulfebw@stanford.edu>
2018-02-10 12:46:00 -0800

    adding clipping to vectorized normalizing env

--- hgail/envs/vectorized_normalized_env.py
+++ hgail/envs/vectorized_normalized_env.py
@@ -2,14 +2,33 @@
 import numpy as np
 
 from rllab.envs.normalized_env import normalize as normalize_env
+from rllab.core.serializable import Serializable
 
 class VectorizedNormalizedEnv(normalize_env):
+
+    def __init__(
+            self,
+            env,
+            clip_std_multiple=np.inf,
+            **kwargs):
+        Serializable.quick_init(self, locals())
+        self.clip_std_multiple = clip_std_multiple
+        super(VectorizedNormalizedEnv, self).__init__(env, **kwargs)
 
     def _update_obs_estimate(self, obs):
         # assert (n_envs, obs_dim) shape, i.e., already flat
         assert len(obs.shape) == 2
         self._obs_mean = (1 - self._obs_alpha) * self._obs_mean + self._obs_alpha * np.mean(obs, axis=0)
         self._obs_var = (1 - self._obs_alpha) * self._obs_var + self._obs_alpha * np.mean(np.square(obs - self._obs_mean), axis=0)
+
+    def _apply_normalize_obs(self, obs):
+        self._update_obs_estimate(obs)
+        std = np.sqrt(self._obs_var) + 1e-8
+        return np.clip(
+            obs - self._obs_mean, 
+            -std * self.clip_std_multiple,
+            std * self.clip_std_multiple
+        ) / std
 
     @property
     def vectorized(self):
